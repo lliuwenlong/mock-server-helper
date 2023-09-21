a0fb46aa1829128a856a08f6e21f2a72d4921307
liuwenlong7 <liuwenlong7@tal.com>
2023-09-21 17:07:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mock-server 可搭配mockjs\@faker-js/faker 生成数据的mockserver
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+
+export default {
+    input: ['./lib/index.js'],
+    output: {
+        file: './dist/bundle.js',
+        format: 'cjs',
+    },
+};
